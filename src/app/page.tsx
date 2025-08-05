9e2e83bd1e4a1c36ade16a689246a33b7f196fdc
Kauã <kaua.simplicio@fecaf.com.br>
2025-08-04 21:52:31 -0300

    chore: update shadcn theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
 
-    <div className="flex justify-between gap-1 items-center bg-amber-300 mx-2 p-2">
+    <div>
       <Button>Click me</Button>
     </div>
   );
