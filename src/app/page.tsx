6eb766b600a8c8511f750513d7b5634c284ef604
Kauã <kaua.simplicio@fecaf.com.br>
2025-08-06 15:34:30 -0300

    feat: add product list and product item components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,43 @@
 import { Header } from "@/components/common/header";
+import ProductList from "@/components/common/product-list";
+import { db } from "@/db";
+import Image from "next/image";
 
-const Home = () => {
+const Home = async () => {
+  const products = await db.query.productTable.findMany({
+    with: {
+      variants: true,
+    },
+  });
+
   return (
     <div>
       <Header />
+      <div className="space-y-6">
+        <div className="px-5">
+          <Image
+            src="/banner01.png"
+            alt="Leve uma vida com estilo"
+            height={0}
+            width={0}
+            sizes="100vw"
+            className="w-full h-auto"
+          />
+        </div>
+
+        <ProductList products={products} title="Mais vendidos" />
+
+        <div className="px-5">
+          <Image
+            src="/banner02.png"
+            alt="Leve uma vida com estilo"
+            height={0}
+            width={0}
+            sizes="100vw"
+            className="w-full h-auto"
+          />
+        </div>
+      </div>
     </div>
   );
 };
---
+++ src/components/common/product-item.tsx
@@ -0,0 +1,36 @@
+import { productTable, productVariantTable } from "@/db/schema";
+import { formatCentstoBRL } from "@/helpers/money";
+import Image from "next/image";
+import Link from "next/link";
+
+interface ProductItemProps {
+  product: typeof productTable.$inferSelect & {
+    variants: (typeof productVariantTable.$inferSelect)[];
+  };
+}
+
+const ProductItem = ({ product }: ProductItemProps) => {
+  const firtsVarient = product.variants[0];
+  return (
+    <Link href="/" className="flex flex-col gap-4">
+      <Image
+        src={firtsVarient.imageUrl}
+        alt={firtsVarient.name}
+        width={150}
+        height={150}
+        className="rounded-3xl"
+      />
+      <div className="flex max-w-[150px] flex-col gap-1">
+        <p className="truncate text-sm font-medium">{product.name}</p>
+        <p className="truncate text-xs text-muted-foreground font-medium">
+          {product.description}
+        </p>
+        <p className="truncate text-sm semi-bold">
+          {formatCentstoBRL(firtsVarient.priceInCents)}
+        </p>
+      </div>
+    </Link>
+  );
+};
+
+export default ProductItem;
